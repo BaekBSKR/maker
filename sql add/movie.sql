923a68a02da26293235fe2cde1582f3f81d51ee2
BaekBSKR <145102084+BaekBSKR@users.noreply.github.com>
2023-10-04 09:55:30 +0900

    줄거리 길이 수정
    SQL > 행, 시퀀스 삭제 후 시퀀즈 재생성 작업 진행
    JAVA > SQL 과정 진행 후 서버 실행

--- sql add/movie.sql
+++ sql add/movie.sql
@@ -1,12 +1,12 @@
 create table movie(
     mno number(10),
     mTitle varchar2(100) not null,
-    mComment varchar2(1000) not null,
+    mComment varchar2(2500) not null,
     mDirect varchar2(40) not null,
     mChar varchar2(40) not null,
     mOpen varchar2(10) not null,
     mStar float,
-    mImg nvarchar2(20),
+    mImg nvarchar2(1000),
     mTime varchar2(5)
 );
 CREATE sequence seq_movie;
@@ -18,5 +18,9 @@ insert into movie(mno, mTitle, mComment, mDirect,
     mChar, mOpen)
 values(seq_movie.nextval, '영화1', '이 영화는...', '감독', '이배우', '20220101');
 
-select * from movie;
-ALTER TABLE movie MODIFY (mTitle VARCHAR2(100));
+select *  from movie;
+
+ALTER TABLE movie MODIFY (mComment VARCHAR2(2500));
+commit;
+
+delete movie;
