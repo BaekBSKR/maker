9273c0524fb968f4f1ef9d8889b5a41fa09e93d4
hubmiller <140375897+hubmiller@users.noreply.github.com>
2023-10-12 17:05:26 +0900

    Add files via upload

---
+++ 1012/NoticeVO.java
@@ -0,0 +1,17 @@
+package com.maker.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+
+@Data
+public class NoticeVO {
+	private long nno;
+	private String n_title;
+	private String n_comment;
+	private Date n_reg;
+	private long cno;
+	private Date n_update;
+	
+}
