8120cebff22d101b8d785c3d079714107f3d6799
BaekBSKR <145102084+BaekBSKR@users.noreply.github.com>
2023-10-12 13:17:28 +0900

    Add files via upload

--- sql add/notice.sql
+++ sql add/notice.sql
@@ -1,8 +1,9 @@
 create table notice(
     nno number(10),
-    n_title varchar2(10) not null,
-    n_comment varchar2(15) not null,
+    n_title varchar2(100) not null,
+    n_comment varchar2(1000) not null,
     n_reg date default sysdate,
+    n_update date default sysdate,
     cno number(10)
 );
 CREATE sequence seq_notice;
@@ -14,5 +15,7 @@ foreign key(cno) references customer(cno);
 
 insert into notice(nno, n_title, n_comment, n_reg, cno)
 values(seq_notice.nextval, '공지1', '00일 부로...', sysdate, 1);
-
+ALTER TABLE notice MODIFY (n_comment VARCHAR2(1000));
+ALTER TABLE notice add n_update date default sysdate;
 select * from notice;
+drop table notice;
--- sql add/ticket.sql
+++ sql add/ticket.sql
@@ -24,3 +24,9 @@ values(seq_ticket.nextval, 35, 1, 1, '10~12', 10000);
 select * from ticket;
 ALTER TABLE ticket MODIFY (t_time VARCHAR2(20));
 commit;
+drop table ticket;
+
+insert into ticket(tno, t_time, t_price, sno, mno, cno) 
+values(seq_ticket.nextval, '2023/10/10/18:00', 10000, 4, 4, 1);
+select sno from ticket where t_time = '2023/10/11/12:00' and mno = 4;
+select * from ticket where cno = 1 order by tno desc;
